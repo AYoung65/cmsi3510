4a6406ae192d4950ea800ccbec8d6ae2d936193b
AYoung65 <122577974+AYoung65@users.noreply.github.com>
2023-12-20 12:16:02 -0800

    Small edits

--- DiningPhilosophers/dinner_party.go
+++ DiningPhilosophers/dinner_party.go
@@ -11,24 +11,19 @@ const EAT_TIMES int = 3
 const NUM_EATING_PHILOSOPHER int = 2
 
 type Host struct {
-	// channel for allowing a philosopher to eat
 	requestChannel chan *Philosopher
-
-	// channel that quits the program; tells the host
-	// to stop hosting
+	
 	quitChannel chan int
 
-	// keep track/bookkeeping of currently eating philosophers
 	eatingPhilosophers map[int]bool
 
-	// we need to lock the bookkeeping variable
 	mu sync.Mutex
 }
 
 func (h *Host) manage() {
 	for {
 		if len(h.requestChannel) == NUM_EATING_PHILOSOPHER {
-			finished := <-h.requestChannel // Pops a Philosopher object off the channel
+			finished := <-h.requestChannel 
 			currentlyEating := make([]int, 0, NUM_PHILOSOPHERS)
 			for index, eating := range h.eatingPhilosophers {
 				if eating {
@@ -40,11 +35,8 @@ func (h *Host) manage() {
 			h.eatingPhilosophers[finished.ID] = false
 		}
 
-		// similar to a switch stmt
 		select {
 		case <-h.quitChannel:
-			// when the channel receives a signal
-			// end the host manage function
 			fmt.Println("party is over")
 			return
 		default:
@@ -63,14 +55,10 @@ func main() {
 		eatingPhilosophers: make(map[int]bool)
 	}
 
-	// make chopsticks
-	// TODO
 	for i := range(
 
-	// make philos9oophers
 	philos := make([]*Philosopher, NUM_PHILOSOPHERS)
 
-	// for i := 0; i < NUM_PHILOSOPHERS; i++ {
 	for i := range(NUM_PHILOSOPHERS) {
 		philos[i] = &Philosopher{
 			ID: i + 1,
